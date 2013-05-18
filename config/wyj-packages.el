710184cd96a842beac386d61eb19abe6bc444350
wyj2046 <wyj2046@gmail.com>
2013-05-18 12:27:24 +0800

    通过customize-variable将google-translate的默认源语言定义为英语, 目标语言定义为中文; 同时增加逆向翻译的快捷键C-c r

--- config/wyj-packages.el
+++ config/wyj-packages.el
@@ -192,3 +192,5 @@ the mode-line."
 (require 'google-translate)
 (global-set-key "\C-ct" 'google-translate-at-point)
 (global-set-key "\C-cT" 'google-translate-query-translate)
+(global-set-key (kbd "C-c r") 'google-translate-at-point-reverse)
+(global-set-key (kbd "C-c R") 'google-translate-query-translate-reverse)
